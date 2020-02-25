0e7b8539099d4b6d504f425fd83c640c71d7a9ba
Saransh Gupta <saranshgupta1995@gmail.com>
2020-02-25 23:12:49 +0530

    add docs for TwoSum

--- Google/TwoSum.js
+++ Google/TwoSum.js
@@ -1,10 +1,13 @@
 var twoSum = function(nums, target) {
+//     have a hashmap to keep track of required targets corresponding to each number
     let data = {};
     let num = comp = -1;
     for(let i=0,l = nums.length;i<l;i++){
         num = nums[i];
         comp = target-num
+//         if required number is found in the hashmap, we found our value
         if(data[comp]!=undefined) return [data[comp], i]
+//         else, save
         data[num] = i;
     }
 };
